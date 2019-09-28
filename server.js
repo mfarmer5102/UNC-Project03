479fd7af0a5bfd17208105e6253f47d24cb1f7d5
Matthew <mfarmer5102@gmail.com>
2019-09-28 12:12:31 -0400

    Retry

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
-app.use(express.static(path.join(__dirname, "build")));
-
 // Routes
 require("./routes/apiRoutes")(app);
 
