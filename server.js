f6734b108f2412581bec864cb9c8e22706f6a6cc
Matthew <mfarmer5102@gmail.com>
2019-09-28 11:41:15 -0400

    Preparing for heroku deploy

--- server.js
+++ server.js
@@ -4,27 +4,24 @@ var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 5000;
+var path = require("path");
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
+app.use(express.static(path.join(__dirname, "build")));
+
 // Routes
 require("./routes/apiRoutes")(app);
 
-//404 Route
+// React SPA Route
 app.get("*", function(req, res) {
-  res.send("hit catch all route");
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 var syncOptions = { force: false, alter: true };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
