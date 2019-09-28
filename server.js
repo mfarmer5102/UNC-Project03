64fdc39004972db0c2791e85da404a05c6e1593b
Matthew <mfarmer5102@gmail.com>
2019-09-28 12:16:21 -0400

    Revising public folder in server

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 var express = require("express");
+var path = require("path");
+var app = express();
+var bodyParser = require("body-parser");
 
+//Import DB models
 var db = require("./models");
 
-var app = express();
-var PORT = process.env.PORT || 5000;
-var path = require("path");
+//Allow post requests to be handled
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+//Serve up the React application
+app.use(express.static(path.resolve(__dirname, "build")));
 
 // Routes
 require("./routes/apiRoutes")(app);
 
 // React SPA Route
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 var syncOptions = { force: false, alter: true };
 
 // Starting the server, syncing our models ------------------------------------/
+var PORT = process.env.PORT || 5000;
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
